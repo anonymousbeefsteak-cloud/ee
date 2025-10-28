89df4a23e81a71acbd9b721f57aaf50a87c96ba9
anonymousbeefsteak-cloud <anonymousbeefsteak@gmail.com>
2025-10-28 12:50:25 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// 环境变量配置
+window.ENV = {
+  LIFF_ID: '2008316489-6nMjb0KX',
+  API_ENDPOINT: 'https://script.google.com/macros/s/AKfycbxR1gzQgV1jq8DUhYX8EY0EmA6i2PBnjo_IZJUuwRgj7Ggjgro8Mdbnic7ZdhIIc2B1/exec',
+  DELIVERY_FEE: 30
+};
